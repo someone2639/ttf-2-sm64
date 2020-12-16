cfbe21301e2e5f046824d4864580703223ac373f
farisawan-2000 <farisawan.2000@gmail.com>
2020-12-15 21:07:27 -0500

    s2d text engine support

--- ttf2png.py
+++ ttf2png.py
@@ -48,19 +48,38 @@ checkArray = [i for i in testString]
 checkArray2=['exclam','period','comma','ampersand','hyphen','questiondown','quotedblleft','quotedblright','percent','tilde',
     'parenleft','parenright','colon','acute']
 
+check_char = "00000000000000000\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2A\x2B\x2C\x2D\x2E\x2F\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3A\x3B\x3C\x3D>\x3F\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x5B\x5C\x5D\x5E\x5F\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x7B\x7C\x7D\x7E\x7F"
+
 testArray= nums+checkArray+checkArray2
 files = os.listdir(TEXTS_DIR)
 ctr = 0
 files2 = files
-for filename in files2:
-    name, ext = os.path.splitext(filename)
-    input_txt = TEXTS_DIR + "/" + filename
+# for filename in files2:
+#     name, ext = os.path.splitext(filename)
+#     input_txt = TEXTS_DIR + "/" + filename
 
-    if name in testArray:
-        ch = filename.split('.')[0]
-        output_png = IMAGES_DIR + "/" + str(testArray.index(ch)) + ".png"
-        os.system(' '.join(["convert", "-interline-spacing 0", "-size 32x32", "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba\(0,0,0,0\)","+antialias", "label:$(cat " + input_txt+")", output_png]))
-        ctr+=1
+#     if name in testArray:
+#         ch = filename.split('.')[0]
+#         ctr+=1
+
+my_width = 16
+my_height = 16
+
+for i in range(len(check_char)):
+    output_png = IMAGES_DIR + "/" + str(i) + ".png"
+    from pathlib import Path
+    Path(output_png).touch()
+    print(i)
+    if i == 32:
+        os.system(' '.join(["convert", "-interline-spacing 0", "-size %dx%d" % (my_width, my_height), "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba\(255,255,255,0\)","+antialias", "label:'\ '", output_png]))
+    elif i == 34:
+        os.system(' '.join(["convert", "-interline-spacing 0", "-size %dx%d" % (my_width, my_height), "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba\(255,255,255,0\)","+antialias", "label:'\"'", output_png]))
+    elif i in[96]:
+        os.system(' '.join(["convert", "-interline-spacing 0", "-size %dx%d" % (my_width, my_height), "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba\(255,255,255,0\)","+antialias","label:\\" + check_char[i], output_png]))
+    elif i in[92]:
+        os.system(' '.join(["convert", "-interline-spacing 0", "-size %dx%d" % (my_width, my_height), "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba\(255,255,255,0\)","+antialias","label:'\\\\'", output_png]))
+    else:
+        os.system(' '.join(["convert", "-interline-spacing 0", "-size %dx%d" % (my_width, my_height), "-font", TTF_PATH, "-pointsize", FONT_SIZE, "-background", "rgba\(255,255,255,0\)","+antialias", "label:\"" + check_char[i]+"\"", output_png]))
     # else:
     #     files.remove(filename)
 
